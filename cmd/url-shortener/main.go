5dd13a4e5f121332a8c016d19896351dd50b249d
ilya1231231 <malygin.ilja@yandex.ru>
2023-07-30 13:42:37 +0300

    Начал писать конфиг и установил библиотеку cleanenv. Нужно изучить детальнее функцию MustLoad

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("working")
+	//данная библиотека минималистичная и может читать почти со всех файлов конфига (json, yaml, env)
+	// TODO: init config: cleanenv
+
+	//библиотека для логирования
+	// TODO: init logger: slog
+
+	//вот тут я бы использовал mysql
+	// TODO: init storage: sqlite
+
+	//
+	// TODO: init router: chi, "chi/render"
+
+	// TODO: run server
 }
