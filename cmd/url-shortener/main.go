3eb66aa3c50f16de632c0560a051df1fb5283e34
ilya1231231 <malygin.ilja@yandex.ru>
2023-08-03 15:36:43 +0300

    дописал логгер, нужно изучить детальнее

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"golang.org/x/exp/slog"
 	"os"
 	"url-shortender/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	//данная библиотека минималистичная и может читать почти со всех файлов конфига (json, yaml, env)
 	// TODO: init config: cleanenv
+	//!!! изучть как правильно используется Setenv
 	os.Setenv("CONFIG_PATH", "./config/local.yaml")
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
 	//библиотека для логирования
-	// TODO: init logger: slog
-
+	//изучить и понять логику детальнее. При env=prod не выводится сообщение из-за настройки.
+	log := setupLogger(cfg.Env)
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 	//вот тут я бы использовал mysql
 	// TODO: init storage: sqlite
 
@@ -22,4 +30,20 @@ func main() {
 	// TODO: init router: chi, "chi/render"
 
 	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		//минимальный уровень выводимых ошибок ошибки до этой константы игнорируются
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
