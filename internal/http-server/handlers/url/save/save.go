7e18237e4c8b07d920d821b8bf4b80c8536cea74
ilya1231231 <malygin.ilja@yandex.ru>
2023-08-15 19:21:03 +0300

    Почти дописал хендлер сохранения. Раскидал todo-шки и использовал валидатор

--- internal/http-server/handlers/url/save/save.go
+++ internal/http-server/handlers/url/save/save.go
@@ -1,12 +1,16 @@
 package save
 
 import (
+	"errors"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
 	"golang.org/x/exp/slog"
+	"math/rand"
 	"net/http"
 	res "url-shortender/internal/http-server/api/response"
 	"url-shortender/internal/lib/sl"
+	"url-shortender/internal/storage"
 )
 
 type Request struct {
@@ -40,11 +44,47 @@ func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
 			log.Error("failed to decode request", sl.Err(err))
 			//@todo изучить render подробнее
 			render.JSON(w, r, res.Error("failed to decode request"))
-			//верхняя функция не преривает работу хендлера, поэтому прериваем его самостоятельно выходом из функции
+			//верхняя функция не прерывает работу хендлера, поэтому прериваем его самостоятельно выходом из функции
 			return
 		}
 
 		log.Info("request body decoded", slog.Any("data", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			//@todo понять как работают ошибки и узнать в каком виде все возвращается
+			validateErrs := err.(validator.ValidationErrors)
+			//@todo сделать user friendly сообщения об ошибке валидации и протестировать. Пока захардкодил
+			log.Info("array of errors", slog.Any("errors", validateErrs))
+			render.JSON(w, r, res.Error("Validation Error"))
+			return
+		}
+
+		alias := req.Alias
+		if alias == "" {
+			//@todo сделать генератор рандомной строки куда мы передаем кол-во символов строки. Пакет random.
+			alias = string(rune(rand.Int()))
+		}
+
+		id, err := urlSaver.CreateUrl(req.Url, alias)
+		if errors.Is(err, storage.ErrURLExists) {
+			log.Info("url already exists", slog.String("URL", req.Url))
+			render.JSON(w, r, res.Error("url already exists"))
+			return
+		}
+
+		if err != nil {
+			//@todo проследить как ошибка поднимается с нижних уровней
+			log.Error("filed to create url", sl.Err(err))
+			render.JSON(w, r, res.Error("filed to create url"))
+			return
+		}
+		log.Info("added new URL", slog.Int64("ID", id))
+
+		//@todo изучить как рабоатют ключи в интерфесах. Так как ключ был задан Response неявно
+		render.JSON(w, r, Response{
+			Response: res.OK(),
+			Alias:    alias,
+		})
 	}
 
 }
